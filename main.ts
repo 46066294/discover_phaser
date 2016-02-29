df2aa5b42eb48f24a51a34cfabc474c000af478b
46066294p <46066294p@iespoblenou.org>
2016-02-29 18:03:39 +0100

    Initial commit....intro background and bicho

---
+++ main.ts
@@ -0,0 +1,40 @@
+/// <reference path="phaser/phaser.d.ts"/>
+
+class mainState extends Phaser.State {
+    game: Phaser.Game;
+
+    preload():void {
+        super.preload();
+        this.game.load.image("player", "assets/player.png");
+    }
+
+    create():void {
+        super.create();
+         var _bgColor = '#099';
+        this.stage.setBackgroundColor(_bgColor)
+        var player = this.game.add.sprite(
+                        this.game.world.centerX,
+                        this.game.world.centerY,
+                        'player');
+    }
+
+    //se ejecuta 60 veces por segundo
+    update():void {
+        super.update();
+    }
+}
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+};
